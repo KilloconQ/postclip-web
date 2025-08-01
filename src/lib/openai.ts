64996a0e26d18efbc7e5726e8becfca0f169ee49
fcorralescarfaxmx <fcorrales@carfaxmx.com.mx>
2025-08-01 14:30:54 -0600

    faet: add openai

---
+++ src/lib/openai.ts
@@ -0,0 +1,10 @@
+import OpenAI from 'openai';
+const client = new OpenAI();
+
+const response = await client.responses.create({
+  model: 'gpt-4.1',
+  input:
+    'Improve this post for X and Bluesky it must be less than 280 characters. Do not use hashtags or emojis. Make it more engaging and concise.',
+});
+
+console.log(response.output_text);
